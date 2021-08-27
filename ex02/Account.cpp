02d143c2b10fc0c5749825595f0220b3a67f0cfb
Ramil <33423497+teregulov@users.noreply.github.com>
2021-08-27 11:20:19 +0300

    ex02 timestamp

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,10 +1,58 @@
 #include "Account.hpp"
 #include <ctime>
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
 
-static void Account::_displayTimestamp()
+
+int counttt = 0;
+
+void Account::_displayTimestamp()
 {
-	std::time_t time;
+    time_t rawtime;
+    struct tm * timeinfo;
+    char buffer [80];
 
-	time = std::time(time);
+    time (&rawtime);
+    timeinfo = localtime (&rawtime);
+    strftime (buffer,80,"[%Y%m%d_%H%M%S]",timeinfo);
+    std::cout << buffer;
+}
+
+
+ void Account::displayAccountsInfos( void )
+{
+    _displayTimestamp();
+    getNbAccounts();
+    getTotalAmount();
 
 }
+
+Account::Account( int initial_deposit )
+{
+    Account Accountqwer;
+    Accountqwer._nbDeposits = initial_deposit;
+    _displayTimestamp();
+    std::cout << " index:" << counttt << ";amount:" << initial_deposit << ";created" << std::endl;
+    counttt++;
+    Account();
+}
+
+Account::Account(void)
+{
+    int index;
+    index = counttt;
+    _accountIndex = index++;
+}
+
+int Account::getNbWithdrawals(void) {
+    return 0;
+}
+
+Account::~Account(void)
+{
+
+}
+
+
+
---
+++ ex02/sand.cpp
@@ -0,0 +1,10 @@
+#include "Account.hpp"
+
+int main (void)
+{
+    Account(23);
+    Account(67);
+    Account(345);
+
+    return 0;
+}
